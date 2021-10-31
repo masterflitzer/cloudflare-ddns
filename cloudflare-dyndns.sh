f1560ff885d3fba26e3b320dc8e94101e4acaae1
Masterflitzer <60170948+Masterflitzer@users.noreply.github.com>
2021-10-31 22:33:02 +0100

    remove unnessasary curly braces around vars

--- cloudflare-dyndns.sh
+++ cloudflare-dyndns.sh
@@ -17,13 +17,13 @@ API_ENDPOINT="https://api.cloudflare.com/client/v4"
 
 # custom API-Token (not global API-Key)
 # permissions needed: #dns_records:edit
-API_TOKEN="$(cat "${CONFIG_FILE}" | grep -E "^API_TOKEN=" | head -1 | cut -d "=" -f2)"
+API_TOKEN="$(cat "$CONFIG_FILE" | grep -E "^API_TOKEN=" | head -1 | cut -d "=" -f2)"
 
 # when you want to update "www.example.com", "www" is the RECORD_NAME and "example.com" is the ZONE_NAME
-ZONE_NAME="$(cat "${CONFIG_FILE}" | grep -E "^ZONE_NAME=" | head -1 | cut -d "=" -f2)"
+ZONE_NAME="$(cat "$CONFIG_FILE" | grep -E "^ZONE_NAME=" | head -1 | cut -d "=" -f2)"
 
-COUNTER_V4="${COUNTER}"
-COUNTER_V6="${COUNTER}"
+COUNTER_V4="$COUNTER"
+COUNTER_V6="$COUNTER"
 
 
 ##### Functions
@@ -46,28 +46,28 @@ printf "\n#=====================================================================
 
 echo -n "Determining IPv4 address"
 links_IPv4
-while test -z "${IP_V4}" -a "${COUNTER_V4}" -gt "0"
+while test -z "$IP_V4" -a "$COUNTER_V4" -gt "0"
 do
     echo -n '.'
-    COUNTER_V4="$(echo "${COUNTER_V4}"|awk '{print $1-1}')"
-    sleep ${INTERVAL}
+    COUNTER_V4="$(echo "$COUNTER_V4"|awk '{print $1-1}')"
+    sleep $INTERVAL
     links_IPv4
 done
 echo
 
 echo -n "Determining IPv6 address"
 links_IPv6
-while test -z "${IP_V6}" -a "${COUNTER_V6}" -gt "0"
+while test -z "$IP_V6" -a "$COUNTER_V6" -gt "0"
 do
     echo -n '.'
-    COUNTER_V6="$(echo "${COUNTER_V6}"|awk '{print $1-1}')"
-    sleep ${INTERVAL}
+    COUNTER_V6="$(echo "$COUNTER_V6"|awk '{print $1-1}')"
+    sleep $INTERVAL
     links_IPv6
 done
 echo
 
-echo "IPv4: ${IP_V4}"
-echo "IPv6: ${IP_V6}"
+echo "IPv4: $IP_V4"
+echo "IPv6: $IP_V6"
 
 printf "\n#==============================================================================#\n\n"
 
@@ -80,21 +80,21 @@ RECORD_ID_V4="$(curl -X GET "${API_ENDPOINT}/zones/${ZONE_ID}/dns_records" -H "A
 ### Get Record ID (IPv6)
 RECORD_ID_V6="$(curl -X GET "${API_ENDPOINT}/zones/${ZONE_ID}/dns_records" -H "Authorization: Bearer ${API_TOKEN}" -H "Content-Type: application/json" 2>/dev/null | jq -r ".result[] | select((.name == \"${ZONE_NAME}\") and (.type == \"AAAA\")) | .id")"
 
-echo "Record Name      = ${ZONE_NAME}"
-echo "ZONE_ID          = ${ZONE_ID}"
-echo "Record ID (IPv4) = ${RECORD_ID_V4}"
-echo "Record ID (IPv6) = ${RECORD_ID_V6}"
+echo "Record Name      = $ZONE_NAME"
+echo "ZONE_ID          = $ZONE_ID"
+echo "Record ID (IPv4) = $RECORD_ID_V4"
+echo "Record ID (IPv6) = $RECORD_ID_V6"
 
 
 ### Set IP
 printf "\n#==============================================================================#\n\n"
 
-echo "IPv4: Updating DNS Record to '${IP_V4}'"
+echo "IPv4: Updating DNS Record to '$IP_V4'"
 curl -X PUT "${API_ENDPOINT}/zones/${ZONE_ID}/dns_records/${RECORD_ID_V4}" -H "Authorization: Bearer ${API_TOKEN}" -H "Content-Type: application/json" --data "{\"type\":\"A\",\"name\":\"${ZONE_NAME}\",\"content\":\"${IP_V4}\",\"ttl\":${TTL},\"proxied\":${PROXIED}}"; echo
 
 printf "\n#------------------------------------------------------------------------------#\n\n"
 
-echo "IPv6: Updating DNS Record to '${IP_V6}'"
+echo "IPv6: Updating DNS Record to '$IP_V6'"
 curl -X PUT "${API_ENDPOINT}/zones/${ZONE_ID}/dns_records/${RECORD_ID_V6}" -H "Authorization: Bearer ${API_TOKEN}" -H "Content-Type: application/json" --data "{\"type\":\"AAAA\",\"name\":\"${ZONE_NAME}\",\"content\":\"${IP_V6}\",\"ttl\":${TTL},\"proxied\":${PROXIED}}"; echo
 
 
@@ -110,10 +110,10 @@ do
 
     RECORD_ID="$(curl -X GET "${API_ENDPOINT}/zones/${ZONE_ID}/dns_records" -H "Authorization: Bearer ${API_TOKEN}" -H "Content-Type: application/json" 2>/dev/null | jq -r ".result[] | select((.name == \"${RECORD_NAME}.${ZONE_NAME}\") and (.type == \"A\")) | .id")"
 
-    echo "Record Name      = ${RECORD_NAME}.${ZONE_NAME}"
-    echo "Record ID        = ${RECORD_ID}"
+    echo "Record Name      = $RECORD_NAME.$ZONE_NAME"
+    echo "Record ID        = $RECORD_ID"
 
-    echo "Updating DNS Record to '${IP_V4}'"
+    echo "Updating DNS Record to '$IP_V4'"
     curl -X PUT "${API_ENDPOINT}/zones/${ZONE_ID}/dns_records/${RECORD_ID}" -H "Authorization: Bearer ${API_TOKEN}" -H "Content-Type: application/json" --data "{\"type\":\"A\",\"name\":\"${RECORD_NAME}.${ZONE_NAME}\",\"content\":\"${IP_V4}\",\"ttl\":${TTL},\"proxied\":${PROXIED}}"; echo
 
     let "COUNTER += 1"
@@ -132,8 +132,8 @@ do
 
     RECORD_ID="$(curl -X GET "${API_ENDPOINT}/zones/${ZONE_ID}/dns_records" -H "Authorization: Bearer ${API_TOKEN}" -H "Content-Type: application/json" 2>/dev/null | jq -r ".result[] | select((.name == \"${RECORD_NAME}.${ZONE_NAME}\") and (.type == \"AAAA\")) | .id")"
 
-    echo "Record Name      = ${RECORD_NAME}.${ZONE_NAME}"
-    echo "Record ID        = ${RECORD_ID}"
+    echo "Record Name      = $RECORD_NAME.$ZONE_NAME"
+    echo "Record ID        = $RECORD_ID"
 
     echo "Updating DNS Record to '${IP_V6}'"
     curl -X PUT "${API_ENDPOINT}/zones/${ZONE_ID}/dns_records/${RECORD_ID}" -H "Authorization: Bearer ${API_TOKEN}" -H "Content-Type: application/json" --data "{\"type\":\"AAAA\",\"name\":\"${RECORD_NAME}.${ZONE_NAME}\",\"content\":\"${IP_V6}\",\"ttl\":${TTL},\"proxied\":${PROXIED}}"; echo
